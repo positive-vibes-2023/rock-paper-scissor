23866b8a5005bad71576b8dfa207edf0acc42f0e
postive-vibes-2023 <47699892+positive-vibes-2023@users.noreply.github.com>
2023-09-05 00:33:26 +0100

    first version of game is done'

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let playerChoice= prompt("please select either rock paper or scissor.");
-console.log(playerChoice);
+
+//define function to get computer choices randomly.
 
 function getComputerChoice(){
     let number=Math.floor(Math.random()*3)+1;
@@ -15,33 +15,116 @@ function getComputerChoice(){
     return number;
 }
 
-function game(playerChoice,computerChoice){
-    var winCount=0;
-    console.log(computerChoice);
+//define function to play rock paper scissor round.
 
+function game(playerChoice,computerChoice){
+    console.log(computerChoice);
+    
+        //if player chooses rock
     if (playerChoice==='rock'){
         if(computerChoice==="rock"){
-            console.log(winCount);
-            return winCount;
+            playerScore=0;
+            computeScore=0;
+            foulEntry=0;
         }
         else if (computerChoice==="paper"){
-            winCount= winCount-1;
-            console.log(winCount);
-            return winCount;
+            playerScore= -1;
+            computerScore=1;
+            foulEntry=0;
         }
-     else {
-        winCount=winCount+1;
-        console.log(winCount);
-        return winCount;
-        
-     }
+        else {
+        playerScore=+1;
+        computerScore=-1;
+        foulEntry=0;
+        }
     }
-    console.log(winCount);
+        //if player chooses paper
+   else if (playerChoice==='paper'){
+        if(computerChoice==="rock"){
+            playerScore=1;
+            computeScore=-1;
+            foulEntry=0;
+        }
+        else if (computerChoice==="paper"){
+            playerScore= 0;
+            computerScore=0;
+            foulEntry=0;
+        }
+        else {
+            playerScore=-1;
+            computerScore=1;
+            foulEntry=0;
+        }
+    }
+            // finally if player chooses scissor
+    else if (playerChoice==='scissor'){
+        if(computerChoice==="rock"){
+            playerScore=-1;
+            computerScore=1;
+            foulEntry=0;
+        }
+        else if (computerChoice==="paper"){
+            playerScore= 1;
+            computerScore=-1;
+            foulEntry=0;
+        }
+        else {
+        playerScore=0;
+        computerScore=0;
+        foulEntry=0;
+        }
+    }
+    else {
+        prompt("you did not select either rock , paper or scissor ! Please select only one of these weapon.");
+        playerScore=0;
+        computerScore=0;
+        foulEntry=1;
+    }
+    
 }
-game(playerChoice,getComputerChoice());
 
-/*function playGame(playerChoice,computerChoice,rounds){
-    let rounds=5;
-    //initiate game function 5 times and each time update winCount and lossCount
+//define function to play five rounds
 
-}*/
+function playRound(){
+    //declare local variables totalPlaterScore and totalComputerScore and update them in each round.
+    var totalPlayerScore=0;
+    var totalComputerScore=0;
+    var totalFoulEntries=0;
+
+    //run the for loop for 5 times.
+
+    for(let round=1;round<=5;round++){
+        console.log(`starting total count is now: ${totalPlayerScore}`);
+        let playerChoice= prompt("please select either rock paper or scissor.");
+        game(playerChoice,getComputerChoice());
+        if (foulEntry===1){
+            totalFoulEntries++;
+            prompt(`You seams to be not interested in this game. This game will be terminated after ${3-totalFoulEntries} more wrong entries !`);
+           
+            if (totalFoulEntries==3){
+                prompt ("You exceeded maximum attempts. The game will now end")
+                return
+            }
+        }
+        
+        totalPlayerScore=playerScore+totalPlayerScore;
+        totalComputerScore=computerScore+totalComputerScore;
+        
+            
+        console.log(`updated total win till now are: ${totalPlayerScore}`);
+    }
+    //check who won and give result in prompt.
+  
+    if(totalComputerScore>totalPlayerScore){
+        prompt("YOU LOST ! Machine are winning the battle !!!");
+        }
+    else if (totalComputerScore===totalPlayerScore){
+        prompt("Its a Draw! Well this not much fun.. Awwww");
+        }
+    else {
+        prompt ("Yippieeee YOU WON! You saved the mankind from machines, you are our HEERO !!");
+            }
+
+}
+
+playRound();
