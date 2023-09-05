eeb525614260d3e79978d4695925d887fd773445
postive-vibes-2023 <gaurav0133@gmail.com>
2023-09-06 00:24:06 +0100

    Cleanup the file
    The JS code cleaned up. Many comment line at the end of the code which
    has nothing to do with actual code were deleted. These comment line were
    mainly entered when the git commit message extensions for visual code
    were being configured and being tested.
    Now these are well configured. The Rewrap extension is working fine by
    automatically wrapping the lines which go beyond 72 characters , thus
    ensuring that the commit message looks clean and is easily readable.
    The Rewrap extension adds a golden line which marks the limit where 72
    characters ends.
    The Git commit message (visual code helper) is a extension which helps
    in some ways (ot so sure how it helps!).
    But overall, I am now satisfied by the git commit message workflow.

--- script.js
+++ script.js
@@ -128,7 +128,3 @@ function playRound(){
 }
 //Initiate the the game and play 5 rounds
 playRound();
-//Just playing around with git to make commit more professional
-// which is best editor !
-//Try one more commit to test new extensions.
-// test commit
